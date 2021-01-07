14b6245a5bf34ae1ce17a3bfd4bd6d522493c5a5
Silke Taylor <staylor@urban.org>
2021-01-07 10:40:28 -0600

    Add model Run
    This update adds a Run model to the Django app 'api/v1'. An object of
    class Run corresponds to one record in the Runs database table.
    
    The reference to the user id is still missing because I had some
    problems to figure out how to add a foreign key field from a module that
    is defined in a different app (Users).

--- WebApp/api/v1/models.py
+++ WebApp/api/v1/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.conf import settings
+#from django.dispatch import receiver
+#from django.db.models.signals import post_save
+#from .models import User
+#from django.apps import apps
+#User = apps.get_model('users', 'User')
 
-# Create your models here.
+class Run(models.Model):
+    RUN_TYPE_CHOICES = [
+        (1, 'OLS'),
+        (2, 'Tabulation'),
+    ]
+
+    run_id = models.IntegerField(primary_key=True, unique=True)
+#    researcher_id = models.ForeignKey(User, on_delete=CASCADE)
+    run_type = models.IntegerField(choices=RUN_TYPE_CHOICES)
+    sanitized_run_input = models.JSONField()
+    display_results_decision = models.BooleanField(default=False)
+    display_results_number = models.IntegerField(default=1)
+    date_time_run_submitted = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.run_id;
+
