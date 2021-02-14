760411fc8e24f8998aa2db0cd3d1c00be43840ef
Silke Taylor <staylor@urban.org>
2021-02-13 21:33:27 -0600

    Implement "post request triggers budget record"
    Adds a post_save signal to create an entry in the budget table
    when a new user is created.

--- WebApp/api/v1/models.py
+++ WebApp/api/v1/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 from django.conf import settings
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from WebApp.users.models import User
+
 
 class Run(models.Model):
     RUN_TYPE_CHOICES = [
@@ -23,6 +27,12 @@ class Budget(models.Model):
     researcher_id = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, primary_key=True)
     total_budget_allocated = models.DecimalField(decimal_places=2, max_digits=10, null=False, default=100)
     total_budget_used = models.DecimalField(decimal_places=2, max_digits=10, null=False, default=0)
+
+@receiver(post_save, sender=User)
+def create_budget(sender, instance, created, **kwargs):
+    if created:
+        Budget.objects.create(researcher_id=instance)
+
 
 class Results(models.Model):
     result_id = models.AutoField(primary_key=True)
