362690eb3196fd4acd0c5fba2d3f12c7a0a9eb9e
Silke Taylor <staylor@urban.org>
2021-06-16 16:09:32 -0500

    Add SwaggerUI
    - Log in and navigate to http://0.0.0.0:8000/swagger/

--- WebApp/urls.py
+++ WebApp/urls.py
@@ -3,17 +3,41 @@ from django.urls import path, re_path, include, reverse_lazy
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.views.generic.base import RedirectView
+
 from rest_framework.routers import DefaultRouter
 from rest_framework.authtoken import views
+from rest_framework import permissions
+
 from .users.views import UserViewSet, UserCreateViewSet
 from django.conf.urls import include, url
 
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+# Swagger
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Validation Server API",
+      default_version='v1',
+      description="API for Validation Server",
+      terms_of_service="https://www.urban.org/terms-service",
+      contact=openapi.Contact(email="validation-server@urban.org"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.IsAuthenticated,),
+)
 
 router = DefaultRouter()
 router.register(r'users', UserViewSet)
 router.register(r'users', UserCreateViewSet)
 
 urlpatterns = [
+    # Swagger
+    url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+
     path('admin/', admin.site.urls),
     path('api/v1/', include(router.urls)),
     path('api-token-auth/', views.obtain_auth_token),
