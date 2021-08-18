28b2b417c78803bb013ac8c4cf61ab64c758f6e8
Silke Taylor <staylorUI@users.noreply.github.com>
2021-08-18 10:21:45 -0500

    Revert "Add permission classes to run and result views"

--- WebApp/api/v1/views.py
+++ WebApp/api/v1/views.py
@@ -122,7 +122,6 @@ class ConfidentialDataResultDetail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = ConfidentialDataResultSerializer
 
 class ConfidentialDataRunList(generics.ListCreateAPIView):
-    permission_classes = [IsAuthenticated, IsOwner]
     serializer_class = ConfidentialDataRunSerializer
 
     def get_queryset(self):
@@ -135,12 +134,10 @@ class ConfidentialDataRunList(generics.ListCreateAPIView):
         return queryset
 
 class ConfidentialDataRunDetail(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = [IsAuthenticated, IsOwner]
     queryset = ConfidentialDataRun.objects.all()
     serializer_class = ConfidentialDataRunSerializer
 
 class SyntheticDataResultList(generics.ListCreateAPIView):
-    permission_classes = [IsAuthenticated, IsOwner]
     serializer_class = SyntheticDataResultSerializer
     
     def get_queryset(self):
@@ -153,12 +150,10 @@ class SyntheticDataResultList(generics.ListCreateAPIView):
         return queryset
 
 class SyntheticDataResultDetail(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = [IsAuthenticated, IsOwner]
     queryset = SyntheticDataResult.objects.all()
     serializer_class = SyntheticDataResultSerializer
 
 class SyntheticDataRunList(generics.ListCreateAPIView):
-    permission_classes = [IsAuthenticated, IsOwner]
     serializer_class = SyntheticDataRunSerializer
 
     # Show all of the runs of a particular command
@@ -172,6 +167,5 @@ class SyntheticDataRunList(generics.ListCreateAPIView):
         return queryset
 
 class SyntheticDataRunDetail(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = [IsAuthenticated, IsOwner]
     queryset = SyntheticDataRun.objects.all()
     serializer_class = SyntheticDataRunSerializer
